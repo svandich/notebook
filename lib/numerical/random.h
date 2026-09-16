ba41082ac9617d769771b2adcb5426d21ef72adf
fleqi <felipecabezascardenas@gmail.com>
2026-09-15 21:59:53 -0300

    fix: indentation...

--- lib/numerical/random.h
+++ lib/numerical/random.h
@@ -9,7 +9,7 @@
 #include <chrono>
 
 mt19937_64 rng(
-		chrono::steady_clock::now().time_since_epoch().count());
+	chrono::steady_clock::now().time_since_epoch().count());
 
 long long rnd(long long l, long long r) {
 	return uniform_int_distribution<long long>(l, r)(rng);
